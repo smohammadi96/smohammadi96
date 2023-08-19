7e330b490501baff2c693306cad147de6c6e3b33
Mohammad H. Sattarian <mh_sattarian@outlook.com>
2023-08-19 23:46:30 +0430

    Add base path for images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: { unoptimized: true },
+  images: { unoptimized: true, path: "/smohammadi96" },
   basePath: "/smohammadi96",
 };
 
